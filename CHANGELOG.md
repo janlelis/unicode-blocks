9ffcef1eb14bd3fd9e11a6d72a965be913162c0a
Jan Lelis <mail@janlelis.de>
2017-06-17 13:52:10 +0200

    Unicode 10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
 ## CHANGELOG
 
+### 1.2.0
+
+* Unicode 10.0
+
 ### 1.1.0
 
 * Support Unicode 9.0
 
 ### 1.0.0
 
-* Inital release
+* Initial release
 
--- lib/unicode/blocks/constants.rb
+++ lib/unicode/blocks/constants.rb
@@ -1,7 +1,7 @@
 module Unicode
   module Blocks
-    VERSION = "1.1.0".freeze
-    UNICODE_VERSION = "9.0.0".freeze
+    VERSION = "1.2.0".freeze
+    UNICODE_VERSION = "10.0.0".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/blocks.marshal.gz').freeze
   end
