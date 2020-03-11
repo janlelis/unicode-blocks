e4a894ea3b56342446f486d851116d7d572c053e
Jan Lelis <hi@ruby.consulting>
2020-03-11 14:13:30 +0100

    Release v1.6.0

--- lib/unicode/blocks/constants.rb
+++ lib/unicode/blocks/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   module Blocks
-    VERSION = "1.5.0"
+    VERSION = "1.6.0"
     UNICODE_VERSION = "13.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/blocks.marshal.gz").freeze
