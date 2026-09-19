821c18e8aba7a0dedb040942624353019ea7f204
Jan Lelis <hi@syntaxtransformation.com>
2026-09-19 15:35:36 +0200

    Update Unicode version number

--- lib/unicode/blocks/constants.rb
+++ lib/unicode/blocks/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module Blocks
     VERSION = "1.11.0"
-    UNICODE_VERSION = "17.0.0"
+    UNICODE_VERSION = "18.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/blocks.marshal.gz").freeze
   end
