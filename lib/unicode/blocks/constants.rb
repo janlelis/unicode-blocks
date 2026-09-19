961c7cd8c28de784750a8bc8b82340e0cf71d6f7
Jan Lelis <hi@syntaxtransformation.com>
2026-09-19 16:04:14 +0200

    Release v1.12.0

--- lib/unicode/blocks/constants.rb
+++ lib/unicode/blocks/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   module Blocks
-    VERSION = "1.11.0"
+    VERSION = "1.12.0"
     UNICODE_VERSION = "18.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/blocks.marshal.gz").freeze
